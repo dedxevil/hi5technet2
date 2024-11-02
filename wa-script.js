a59149de45a9d2df213e8b45b271757ce38c2a52
Subash Chandran <schandran@axidio.com>
2024-11-02 11:45:48 +0530

    changed mob number

--- wa-script.js
+++ wa-script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     // WhatsApp numbers
-    const indianNumber = '918667842296';
+    const indianNumber = '919360572528';
     const internationalNumber = '601127728901';
 
     // Elements
